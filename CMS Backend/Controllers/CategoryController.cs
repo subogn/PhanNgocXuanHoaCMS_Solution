aa56e512880861e6cf3c0a38c5df812ad516908c
subogn <Hoap54707@gmail.com>
2026-05-26 10:12:35 +0700

    first commit

---
+++ CMS Backend/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data;
+using CMS.Data.Entities;
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var data = _context.Categories.ToList();
+        return View(data);
+    }
+}
